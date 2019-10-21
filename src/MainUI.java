068705b5ad2a3184755c1079ec99c3f21985fc50
blaisewang <kaito.kaito.k@gmail.com>
2019-10-22 02:28:30 +0800

    Add basic stage

---
+++ src/MainUI.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainUI extends Application {
+
+
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

e2244a6abc3b666fbe4c0487fee21777cc9397ae
blaisewang <kaito.kaito.k@gmail.com>
2019-10-31 19:16:06 +0800

    Extract shared methods

--- src/main/java/Utilities.java
+++ src/main/java/Utilities.java
@@ -1,7 +1,14 @@
 import javafx.scene.image.Image;
 import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 class Utilities {
+
 
     static Image getClipboardImage() {
 
@@ -14,6 +21,27 @@ class Utilities {
         }
 
         return null;
+
+    }
+
+    static void putStringIntoClipboard(String text) {
+        ClipboardContent content = new ClipboardContent();
+        content.putString(text);
+        Clipboard.getSystemClipboard().setContent(content);
+    }
+
+    static OCRRequest.Response concurrentCall(Image image) {
+
+        Recognition recognition = new Recognition(image);
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<OCRRequest.Response> result = executor.submit(recognition);
+
+        try {
+            return result.get();
+        } catch (InterruptedException | ExecutionException e) {
+            return null;
+        }
 
     }
 
